dbd08f24d971367f1604478894a62bdd632f55b3
hwsouch <76270493+hwsouch@users.noreply.github.com>
2021-01-24 09:53:12 +0900

    新年のあいさつを追加した気 がする

---
+++ torain.js
@@ -0,0 +1,38 @@
+let ar = []
+
+for(let i = 1; i <= 100; i = i+1){
+    ar.push (i);
+} 
+
+let sum = 0;
+
+ar.forEach((v) =>  {
+    sum = sum + v ;
+})
+
+console.log(ar);
+
+// ar.push (1);
+
+
+// ar.push (100);
+
+// ar.forEach((v) => {
+//     console.log(v);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ train.js
@@ -0,0 +1,22 @@
+let c = ["I'm Sleepy","遊ぶ"];
+let ar = [c , c , c , c];
+
+ar.forEach((v) => {
+    console .log(v);
+});
+
+let array1 = [ "遊ばない" , "苦しい", "大嫌い" ] ; 
+
+array1.forEach((v) =>
+ {console.log(v);
+});
+
+let nero="海と"
+let asobu="太陽が大好きな列車"
+let a = nero;
+
+let b = asobu;
+
+console.log("海と太陽が大好きな列車")
+
+console.log(  a + b  );
